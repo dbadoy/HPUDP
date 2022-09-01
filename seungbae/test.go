2f9b7651c36cebf8b5d7090e02d49bfef8d7eb40
dbadoy4874 <dbadoy4874@gmail.com>
2022-09-01 18:36:17 +0900

    test: temp, to be remove

--- seungbae/test.go
+++ seungbae/test.go
@@ -7,58 +7,76 @@ import (
 	"net"
 )
 
-func main() {
-	client, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8753})
+func Ping(conn *net.UDPConn) {
+	packet := new(hpudp.PingPacket)
+	packet.SetKind(hpudp.Ping)
+
+	b, err := json.Marshal(&packet)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// packet := new(hpudp.PingPacket)
-	// packet.SetKind(hpudp.Ping)
+	pb := make([]byte, len(b)+2)
+	if len(b) > 256 {
+		return
+	}
+	pb[0] = byte(len(b))
+	pb[1] = hpudp.Ping
+	copy(pb[2:], b[:])
 
-	// b, err := json.Marshal(&packet)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	fmt.Println(b, pb)
 
-	// pb := make([]byte, len(b)+2)
-	// if len(b) > 256 {
-	// 	return
-	// }
-	// pb[0] = byte(len(b))
-	// pb[1] = hpudp.Ping
-	// copy(pb[2:], b[:])
+	if _, err = conn.WriteToUDP(pb, &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8751}); err != nil {
+		fmt.Println(err)
+	}
+	//
+	res := make([]byte, 256)
+	_, _ = conn.Read(res)
 
-	// fmt.Println(b, pb)
+	m := make([]byte, res[0])
+	copy(m, res[2:])
 
-	// if _, err = client.WriteToUDP(pb, &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8751}); err != nil {
-	// 	fmt.Println(err)
-	// }
+	var in hpudp.PongPacket
+	json.Unmarshal(m, &in)
+	fmt.Println(in)
+}
 
-	// packet2 := new(hpudp.JoinPacket)
-	// packet2.SetKind(hpudp.Join)
-	// packet2.NickName = "seungbae"
+func Join(conn *net.UDPConn) {
+	packet2 := new(hpudp.JoinPacket)
+	packet2.SetKind(hpudp.Join)
+	packet2.NickName = "seungbae"
 
-	// b2, err := json.Marshal(&packet2)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	b2, err := json.Marshal(&packet2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// pb2 := make([]byte, len(b2)+2)
-	// if len(b2) > 256 {
-	// 	fmt.Println("oops")
-	// 	return
-	// }
-	// pb2[0] = byte(len(b2))
-	// pb2[1] = hpudp.Join
-	// copy(pb2[2:], b2[:])
-	// if _, err = client.WriteToUDP(pb2, &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8751}); err != nil {
-	// 	fmt.Println(err)
-	// }
+	pb2 := make([]byte, len(b2)+2)
+	if len(b2) > 256 {
+		fmt.Println("oops")
+		return
+	}
+	pb2[0] = byte(len(b2))
+	pb2[1] = hpudp.Join
+	copy(pb2[2:], b2[:])
+	if _, err = conn.WriteToUDP(pb2, &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8751}); err != nil {
+		fmt.Println(err)
+	}
+	//
+	res := make([]byte, 256)
+	_, _ = conn.Read(res)
 
+	m := make([]byte, res[0])
+	copy(m, res[2:])
+
+	var in hpudp.JoinPacket
+	json.Unmarshal(m, &in)
+	fmt.Println(in)
+}
+
+func Find(conn *net.UDPConn) {
 	packet3 := new(hpudp.FindPacket)
 	packet3.SetKind(hpudp.Find)
 	packet3.FindID = hpudp.ID([16]byte{82, 253, 252, 7, 33, 130, 101, 79, 22, 63, 95, 15, 154, 98, 29, 114})
@@ -77,7 +95,26 @@ func main() {
 	pb3[0] = byte(len(b3))
 	pb3[1] = hpudp.Find
 	copy(pb3[2:], b3[:])
-	if _, err = client.WriteToUDP(pb3, &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8751}); err != nil {
+	if _, err = conn.WriteToUDP(pb3, &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8751}); err != nil {
 		fmt.Println(err)
 	}
+	//
+	res := make([]byte, 256)
+	_, _ = conn.Read(res)
+
+	m := make([]byte, res[0])
+	copy(m, res[2:])
+
+	var in hpudp.FindPacket
+	json.Unmarshal(m, &in)
+	fmt.Println(in)
+}
+
+func main() {
+	client, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IP{127, 0, 0, 1}, Port: 8753})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	Ping(client)
 }
